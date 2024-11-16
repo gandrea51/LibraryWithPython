47f7aa16d4f8688bae7b176b9a33e75b3e32d0ae
Andrea Gabrieli <andreag2613@gmail.com>
2024-11-16 15:37:51 +0100

    New Configuration

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config: 
     SECRET_KEY = os.getenv('SECRET_KEY', 'why would I tell you my secret key?')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://andrea:password@localhost/Biblioteca')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://andrea:password@localhost/library')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
